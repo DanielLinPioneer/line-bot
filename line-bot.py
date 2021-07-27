29ccc63d88e5ee2623c9808a3e2da8fbf64cd5f7
Daniel Lin <daniellin2227@student.kss.tp.edu.tw>
2021-07-27 22:34:23 +0800

    make it better

--- line-bot.py
+++ line-bot.py
@@ -34,12 +34,82 @@ def callback():
 
     return 'OK'
 
-
+i []
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
+    msg = event.message.text
+    r = '我不了解你在說甚麼DX'
+
+#你好的不同回答
+    if msg == 'hi':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'Hi':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'hello':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'hello':
+        r = 'Hello!你好啊?我的朋友'
+
+    elif msg == 'hi?':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'Hi?':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'hello?':
+        r = 'Hello!你好啊?我的朋友'
+    elif msg == 'hello?':
+        r = 'Hello!你好啊?我的朋友'
+
+    elif msg == '你好':
+        r = '你好啊?哈哈'
+    elif msg == '你好嗎':
+        r = '我很好, 你?'
+    elif msg == '你好嗎?':
+        r = '我很好, 你?'
+    elif msg == '也好':
+        r = '那就好'
+    elif msg == '很好':
+        r = '那就好'
+    elif msg == '也很好':
+        r = '那就好'
+
+    elif msg == '不好':
+        r = '怎麼了嗎?我的朋友'
+    elif msg == '不太好':
+        r = '怎麼了嗎?我的朋友'
+    elif msg == '你好嗎?':
+        r = '我很好, 你?'
+
+    elif msg == '還好':
+        r = '那就好我的朋友'
+    elif msg == '很好':
+        r = '那就好我的朋友'
+    elif msg == '也很好':
+        r = '那就好我的朋友'
+
+    elif msg == '你好厲害':
+        r = '謝謝'
+    elif msg == '嘿':
+        r = '??'
+    elif msg == '嘿嘿':
+        r = '嗯?'
+
+    elif msg == '你幾歲?':
+        r = '我14歲喔'
+    elif msg == '你幾歲':
+        r = '我14歲啦哈哈'
+
+
+
+#回答你是誰
+    elif msg == '你是誰':
+        r = '我是虛擬語聖DX'
+    elif msg == '你是誰?':
+        r = '我是虛擬語聖DX'
+    
+
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=event.message.text))
+        TextSendMessage(text=r))
 
 
 if __name__ == "__main__":
