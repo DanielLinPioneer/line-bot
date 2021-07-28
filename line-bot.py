8811193982550fc6c1e6d9c9e54dfcf74e7c1823
Daniel Lin <daniellin2227@student.kss.tp.edu.tw>
2021-07-28 09:43:11 +0800

    make it better

--- line-bot.py
+++ line-bot.py
@@ -38,58 +38,56 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     msg = event.message.text
-    r = '我不了解你在說甚麼DX'
+#     r = '我不了解你在說甚麼DX'
 
-#你好的不同回答
-    if msg in ['hi', 'Hi', 'hello', 'Hello', 'hi?', 'Hi?', 'hello?', 'Hello?', '嗨嗨', '嗨'] :
-        r = 'Hello!你好啊?我的朋友, 對了你平常都做甚麼來打發時間阿？'
+# #你好的不同回答
+#     if msg in ['hi', 'Hi', 'hello', 'Hello', 'hi?', 'Hi?', 'hello?', 'Hello?', '嗨嗨', '嗨'] :
+#         r = 'Hello!你好啊?我的朋友, 對了你平常都做甚麼來打發時間阿？'
 
-    elif msg == '你好':
-        r = '你好啊?哈哈'
-    elif msg in ['你好嗎', '你好嗎?']:
-    elif msg in ['也好', '很好', '也很好']:
-        r = '那就好'
+#     elif msg == '你好':
+#         r = '你好啊?哈哈'
+#     elif msg in ['你好嗎', '你好嗎?']:
+#     elif msg in ['也好', '很好', '也很好']:
+#         r = '那就好'
 
-    elif msg in ['不好', '不太好']:
-        r = '怎麼了嗎?我的朋友'
-    elif msg == '你好嗎?':
-        r = '我很好, 你?'
+#     elif msg in ['不好', '不太好']:
+#         r = '怎麼了嗎?我的朋友'
+#     elif msg == '你好嗎?':
+#         r = '我很好, 你?'
 
-    elif msg in ['還好', '很好', '也很好', '都好']:
-        r = '那就好我的朋友'
-    elif msg in ['嗯', '滿意', '很滿意']:
-        r = '那就好我的朋友'
-    elif msg == '你好厲害':
-        r = '謝謝'
-    elif msg == '嘿':
-        r = '??'
-    elif msg == '嘿嘿':
-        r = '嗯?'
+#     elif msg in ['還好', '很好', '也很好', '都好']:
+#         r = '那就好我的朋友'
+#     elif msg in ['嗯', '滿意', '很滿意']:
+#         r = '那就好我的朋友'
+#     elif msg == '你好厲害':
+#         r = '謝謝'
+#     elif msg == '嘿':
+#         r = '??'
+#     elif msg == '嘿嘿':
+#         r = '嗯?'
 
-    elif '幹' in msg:
-        r = '這樣不好喔'
-    elif '三小' in msg:
-        r = '甚麼拉哈哈'
+#     elif '幹' in msg:
+#         r = '這樣不好喔'
+#     elif '三小' in msg:
+#         r = '甚麼拉哈哈'
 
-    elif '幾歲' in msg:
-        r = '我14歲啦哈哈'
-    elif msg == '你幾歲':
-        r = '我14歲啦哈哈'
-    elif '是不是' in msg:
-        r = '痾..不是'
-    elif '不知道' in msg:
-        r = '沒關西, 我的朋友'    
+#     elif '幾歲' in msg:
+#         r = '我14歲啦哈哈'
+#     elif '是不是' in msg:
+#         r = '痾..不是'
+#     elif '不知道' in msg:
+#         r = '沒關西, 我的朋友'    
 
 
-#回答你是誰
-    elif '你是誰' in msg:
-        r = '我是虛擬語聖DX'
-    elif '你好爛' in msg:
-        r = '我還不算是一個好的機器人, 請盡量'    
+# #回答你是誰
+#     elif '你是誰' in msg:
+#         r = '我是虛擬語聖DX'
+#     elif '你好爛' in msg:
+#         r = '我還不算是一個好的機器人, 請盡量'    
 
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=r))
+        TextSendMessage(text=msg))
 
 
 if __name__ == "__main__":
