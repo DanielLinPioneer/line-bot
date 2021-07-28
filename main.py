a0bec03ed14c258438e2179999034265f998abf5
Daniel Lin <daniellin2227@student.kss.tp.edu.tw>
2021-07-28 12:06:07 +0800

    make it better

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    print('Daniel: app Flask - ok')
+    print('Daniel: app Flask ============  - ok')
     msg = event.message.text
     r = '我不了解你在說甚麼DX'
 
