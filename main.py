eb182ebe71299c5e22f2530a13ad03a5af78c210
Daniel Lin <daniellin2227@student.kss.tp.edu.tw>
2021-07-29 10:53:18 +0800

    make it better

--- main.py
+++ main.py
@@ -63,7 +63,7 @@ def handle_message(event):
     elif '無' in msg:
         r = '是嗎?' 
 
-    elif msg in ['還好', '很好', '也很好', '都好', '嗯', '滿意', '很滿意']:
+    elif msg in ['還好', '很好', '也好', '都好', '嗯', '滿意', '很滿意', '我很好']:
         r = '那就好我的朋友'
 
     elif '厲害' in msg:
